ade621d60a95dfca65deeee6e5ef757750d3969d
Erik Schierboom <erik_schierboom@hotmail.com>
2017-04-08 11:37:49 +0200

    Refactored code fix provider

---
+++ xUnit.CodeAnalysis/xUnit.CodeAnalysis/CodeFixProvider.FactWithParameters.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace xUnit.CodeAnalysis
+{
+    public partial class XUnitCodeAnalysisCodeFixProvider
+    {
+        private const string FactWithParametersCodeFixTitle = "Remove parameters";
+
+        private static CodeAction CreateFactWithParametersCodeAction(CodeFixContext context, MethodDeclarationSyntax declaration) 
+            => CodeAction.Create(
+                title: FactWithParametersCodeFixTitle,
+                createChangedDocument: c => RemoveParameters(context.Document, declaration, c),
+                equivalenceKey: FactWithParametersCodeFixTitle);
+
+        private static async Task<Document> RemoveParameters(Document document, MethodDeclarationSyntax methodDeclaration, CancellationToken cancellationToken)
+        {
+            var parameterListWithoutParameters = methodDeclaration.ParameterList.WithParameters(new SeparatedSyntaxList<ParameterSyntax>());
+            var methodDeclarationWithoutParameters = methodDeclaration.WithParameterList(parameterListWithoutParameters);
+
+            var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken);
+            var updatedSyntaxRoot = syntaxRoot.ReplaceNode(methodDeclaration, methodDeclarationWithoutParameters);
+
+            return document.WithSyntaxRoot(updatedSyntaxRoot);
+        }
+    }
+}
--- xUnit.CodeAnalysis/xUnit.CodeAnalysis/CodeFixProvider.cs
+++ xUnit.CodeAnalysis/xUnit.CodeAnalysis/CodeFixProvider.cs
@@ -1,56 +1,41 @@
 ﻿using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace xUnit.CodeAnalysis
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(XUnitCodeAnalysisCodeFixProvider)), Shared]
-    public class XUnitCodeAnalysisCodeFixProvider : CodeFixProvider
+    public partial class XUnitCodeAnalysisCodeFixProvider : CodeFixProvider
     {
-        private const string FactWithParametersCodeFixTitle = "Remove parameters";
-
         public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(XUnitCodeAnalysisAnalyzer.FactWithParametersDiagnosticId);
 
-        public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-            
             var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var methodDeclaration = await GetMethodDeclarationSyntax(context, diagnostic);
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
-
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: FactWithParametersCodeFixTitle,
-                    createChangedDocument: c => RemoveParameters(context.Document, declaration, c),
-                    equivalenceKey: FactWithParametersCodeFixTitle),
-                diagnostic);
+            switch (diagnostic.Descriptor.Id)
+            {
+                case XUnitCodeAnalysisAnalyzer.FactWithParametersDiagnosticId:
+                    context.RegisterCodeFix(CreateFactWithParametersCodeAction(context, methodDeclaration), diagnostic);
+                    break;
+            }
         }
 
-        private async Task<Document> RemoveParameters(Document document, MethodDeclarationSyntax methodWithParameters, CancellationToken cancellationToken)
+        private static async Task<MethodDeclarationSyntax> GetMethodDeclarationSyntax(CodeFixContext context, Diagnostic diagnostic)
         {
-            var parameterListWithoutParametrs = methodWithParameters.ParameterList.WithParameters(new SeparatedSyntaxList<ParameterSyntax>());
-            var methodWithoutParameters = methodWithParameters.WithParameterList(parameterListWithoutParametrs);
-
-            var root = await document.GetSyntaxRootAsync(cancellationToken);
-            var updatedRoot = root.ReplaceNode(methodWithParameters, methodWithoutParameters);
-
-            return document.WithSyntaxRoot(updatedRoot);
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            return root.FindToken(diagnostic.Location.SourceSpan.Start)
+                .Parent
+                .AncestorsAndSelf()
+                .OfType<MethodDeclarationSyntax>()
+                .First();
         }
     }
 }
